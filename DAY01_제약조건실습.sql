3a087654c591997935945439107650a8c6c5b270
han chang hee <hupo119@naver.com>
2023-06-13 00:50:18 +0900

    0612_햇던거 파일추가 제약조건전까지

---
+++ DAY01_제약조건실습.sql
@@ -0,0 +1,45 @@
+-- 제약조건 실습
+-- 테이블 생성 (DDL - CREATE, ALTER, DROP)
+CREATE TABLE USER_NO_CONSTRAINT (
+  USER_NO NUMBER,
+  USER_ID VARCHAR2(20),
+  USER_PWD VARCHAR2(30),
+  USER_NAME VARCHAR2(30),
+  USER_GENDER VARCHAR2(10),
+  USER_PHONE VARCHAR2(30),
+  USER_EMAIL VARCHAR2(50)
+);
+
+-- 데이터 추가
+INSERT INTO USER_NO_CONSTRAINT VALUES(1, 'khuser01', 'pass01', '일용자', '남', '01098774090', 'khuser01@iei.or.kr');
+-- 데이터 조회
+SELECT * FROM USER_NO_CONSTRAINT;
+COMMIT;
+INSERT INTO USER_NO_CONSTRAINT VALUES(2, 'khuser02', 'pass02','이용자','남','01098774090','khuser01@iei.or.kr');
+INSERT INTO USER_NO_CONSTRAINT VALUES(3, null, null, null,'남','01098774090','khuser01@iei.or.kr');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
